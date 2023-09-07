3e4d989dd63c4b4498c79ed6abd22e6af482e647
muhammadganny <mohammadganny@gmail.com>
2023-09-07 15:33:42 +0500

    a

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const isAdmin = require('../middlewares/isAdmin');
+const isAdmin = require('../Middleware/isAdmin');
 const AdminController = require('../controllers/AdminController');
 
 
